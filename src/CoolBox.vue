55a34b057d732801d87e228ce3b16baf96a030bf
galnegus <tobiashindersson@hotmail.com>
2018-01-25 17:52:39 +0100

    Add some linters, an initial build to see if webpack is working

---
+++ src/CoolBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="cool-box">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cool-box',
+};
+</script>
+
+<style lang="scss" scoped>
+.cool-box {
+  width: 400px;
+  height: 400px;
+  background: #f0c;
+}
+</style>
